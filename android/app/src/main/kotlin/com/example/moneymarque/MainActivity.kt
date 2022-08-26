03120b371eb402bc7222b7f22524e0951994819f
Ogaraku Ugochukwu <justineugoogaraku@gmail.com>
2022-08-26 06:59:30 +0100

    setup

---
+++ android/app/src/main/kotlin/com/example/moneymarque/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.moneymarque
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/utils/colors/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+Color kWhite = const Color(0xffffffff);
+Color kBlack = const Color(0xff000000);
+Color kBlueLight = const Color(0xff91D8F7);
+Color kBlue = const Color(0xff3E4095);
+Color kGreen = const Color(0xff42D579);
+Color kGrey = const Color(0xffF8F8F8);
+
+
+Color KBottomNav = const Color(0xffCCCCCC);
+Color KCredit = const Color(0xff00AFEF);
+Color KDebit = const Color(0xffF75C35);
